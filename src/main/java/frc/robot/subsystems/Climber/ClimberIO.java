ab277040d2c8fadbe9ebcc46b1052fd98505e3bc
jasondaming <jasondaming@gmail.com>
2025-02-01 23:27:30 -0600

    add climber & intake

---
+++ src/main/java/frc/robot/subsystems/Climber/ClimberIO.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.Climber;
+
+import frc.robot.subsystems.GenericMotionProfiledSubsystem.GenericMotionProfiledSubsystemIO;
+
+public interface ClimberIO extends GenericMotionProfiledSubsystemIO {}
